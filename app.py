0b28702fdfb122e4ef20ffb8430dbab4c8d9de3b
kuma-superbear <0419120@gmail.com>
2019-12-12 05:55:21 +0900

    commita@201912120554

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import io
 import time
 import numpy as np
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session, jsonify
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session, jsonify, abort
 from werkzeug import secure_filename
 import json
 # call other api
@@ -16,18 +16,42 @@ app.config["JSON_AS_ASCII"] = False
 def index():
   return 'Hello World!'
 
-# get spot information
-@app.route('/get/spot', methods=['GET'])
+
+# get spot recommend
+@app.route('/get/recommend', methods=['GET'])
 def get_spot_information():
   latitude = request.args.get("latitude")
   longitude = request.args.get("longitude")
-  category = request.args.get("category")
-  distance = request.args.get("distance")
-
-  response = get_spot.api_get(latitude, longitude, category, distance)
-
+  # ms
+  seconds = request.args.get("seconds")
+  depature_time = request.args.get("departure_time")
+  response = get_spot.recommend_spot(latitude, longitude, seconds, depature_time)
+  
+  if not response:
+    abort(404, {'code': 'Not Found', 'message': 'spot not found'})
   return jsonify(response)
 
 
+# get route
+@app.route('/get/route', methods=['GET'])
+def get_spot_route():
+  
+  if not response:
+    abort(404, {'code': 'Not Found', 'message': 'spot not found'})
+  return jsonify(response)
+
+
+# error
+@app.errorhandler(400)
+@app.errorhandler(404)
+def error_handler(error):
+    # error.code: HTTPステータスコード
+    return jsonify({'error': {
+        'code': error.description['code'],
+        'message': error.description['message']
+    }}), error.code
+
+
+# main
 if __name__ == '__main__':
   app.run(debug = True)
--- get_spot.py
+++ get_spot.py
@@ -4,40 +4,123 @@ import json
 import sys
 import codecs
 import json
+import math
 
 
-def api_get(latitude, longitude, category, distance):
+def recommend_spot(latitude, longitude, seconds, departure_time):
+  recommend_list = []
+  distance = caluculate_distance(float(seconds))
 
-  apikey = 'Vu5iede1ahziequ3Ang1Asailahhez9D'
-  url = 'https://api.ottop.org/tourist/spots?'
+  # get ottop api
+  ottop_list = get_ottop_api(latitude, longitude, distance)
+  # get yahoo api
+  # N/A
+  # get gurunabi api
+  gurunabi_list = get_gurunabi_api(latitude, longitude, distance)
+
+  recommend_list = ottop_list + gurunabi_list
+
+  return recommend_list
+
+
+def caluculate_distance(seconds):
+  # 係数(km/ms)
+  coefficient  = 30/360000
+  distance = math.floor(seconds * coefficient)
+  return distance
+
+
+def get_gurunabi_api(latitude, longitude, distance):
+  apikey = '21d4e3248fb7645bb26293edd8f81beb'
+  url = 'https://api.gnavi.co.jp/RestSearchAPI/v3/?'
+
+  if distance < (0.3 + 0.5)/2:
+    dis_range = 1
+  elif distance >= (0.3 +0.5)/2 and distance < (0.5 + 1)/2:
+    dis_range = 2
+  elif distance >= (0.5 + 1)/2 and distance < (1 + 2)/2:
+    dis_range = 3
+  elif distance >= (1 + 2)/2 and distance < (2 + 3)/2:
+    dis_range = 4
+  elif distance >= 3:
+    dis_range = 5
+
   param = {
+      'keyid' : apikey,
       'latitude' : latitude,
       'longitude' : longitude,
-      'category' : category,
-      'distance' : distance,
-      'apiKey' : apikey
+      'range' : dis_range
   }
+
   param_str = urllib.parse.urlencode(param)
   read_obj = urllib.request.urlopen(url + param_str)
   raw_data = read_obj.read()
   json_data = json.loads(raw_data)
+  gurunabi_list = extract_gurunabi(json_data)
 
-  dic = extract_ottop(json_data)
-#  json_file = open('spot.json', 'w')
-#  json.dump(json_data, indent=2, ensure_ascii=False)
+  return gurunabi_list
 
-  return dic
+
+def extract_gurunabi(json_data):
+  i = 0
+  gurunabi_dic = {}
+  gurunabi_list = []
+
+  while i < len(json_data):
+    name = json_data['rest'][i]['name'] 
+    url = json_data['rest'][i]['url']
+    latitude = json_data['rest'][i]['latitude']
+    longitude = json_data['rest'][i]['longitude']
+    # mili second
+    stay_time = '360000'
+    description = ''
+    category = '飲食店'
+    gurunabi_dic[i] = {"name" : name, "url" : url, "latitude" : latitude, "longitude" : longitude, 
+    "category" : category, "stay_time" : stay_time, "description" : description }
+    gurunabi_list.append(gurunabi_dic[i])
+    i += 1
+  
+  return gurunabi_list
+
+
+def get_ottop_api(latitude, longitude, distance):
+  apikey = 'Vu5iede1ahziequ3Ang1Asailahhez9D'
+
+  url = 'https://api.ottop.org/tourist/spots?'
+  param = {
+      'latitude' : latitude,
+      'longitude' : longitude,
+      'category' : ' ',
+      'distance' : distance,
+      'apiKey' : apikey
+  }
+
+  param_str = urllib.parse.urlencode(param)
+  read_obj = urllib.request.urlopen(url + param_str)
+  raw_data = read_obj.read()
+  json_data = json.loads(raw_data)
+  ottop_list = extract_ottop(json_data)
+
+  return ottop_list
+
 
 def extract_ottop(json_data):
   i = 0
-  lis = []
   ottop_dic = {}
+  ottop_list = []
+
   while i < len(json_data):
     name = json_data[i]['name']
     url = json_data[i]['url']
     latitude = json_data[i]['geo']['latitude']
     longitude = json_data[i]['geo']['longitude']
-    ottop_dic[i] = {"name" : name, "url" : url, "latitude" : latitude, "longitude" : longitude}
-#    lis.append(ottop_dic[i])
+    # mili second
+    stay_time = '360000'
+    description = ''
+    category = '観光地'
+    ottop_dic[i] = {"name" : name, "url" : url, "latitude" : latitude, "longitude" : longitude,
+    "category" : category, "stay_time" : stay_time, "description" : description }
+    ottop_list.append(ottop_dic[i])
     i += 1
-  return ottop_dic
+  
+  return ottop_list
