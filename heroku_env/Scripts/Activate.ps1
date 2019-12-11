c003926559de7711af8da1f120ef11028325e381
kuma-superbear <0419120@gmail.com>
2019-12-11 10:36:35 +0900

    commit@201912111036

--- heroku_env/Scripts/Activate.ps1
+++ heroku_env/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\ryotaro\OneDrive\ottopHackathon\todxApi\todxApi\heroku_env"
+$env:VIRTUAL_ENV="C:\Users\ryotaro\onedrive\ottopHackathon\todxApi\todxApi\heroku_env"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
