199b3cd8d454362780c0bd98c4081e708a560719
kuma-superbear <0419120@gmail.com>
2019-12-12 10:26:20 +0900

    commit@201912121026

--- app.py
+++ app.py
@@ -11,8 +11,16 @@ import callApi
 import get_spot
 
 app = Flask(__name__)
-CORS(app)
 app.config["JSON_AS_ASCII"] = False
+CORS(app)
+
+# 追加
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 # for test
 @app.route('/')
